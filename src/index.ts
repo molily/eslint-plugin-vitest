7a9fa970ea58ccb9ac56c76029f72f6f5aad5250
Verite Mugabo <mugaboverite@gmail.com>
2023-07-09 18:23:11 +0200

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,7 @@ import preferTodo, { RULE_NAME as preferTodoName } from './rules/prefer-todo'
 import preferSpyOn, { RULE_NAME as preferSpyOnName } from './rules/prefer-spy-on'
 import preferComparisonMatcher, { RULE_NAME as preferComparisonMatcherName } from './rules/prefer-comparison-matcher'
 import preferToContain, { RULE_NAME as preferToContainName } from './rules/prefer-to-contain'
+import unboundMethod, { RULE_NAME as unboundMethodName } from './rules/unbound-method'
 
 const createConfig = (rules: Record<string, string>) => ({
 	plugins: ['vitest'],
@@ -99,9 +100,9 @@ const allRules = {
 	[requireHookName]: 'warn',
 	[preferTodoName]: 'warn',
 	[preferSpyOnName]: 'warn',
-	[preferTodoName]: 'warn',
 	[preferComparisonMatcherName]: 'warn',
-	[preferToContainName]: 'warn'
+	[preferToContainName]: 'warn',
+	[unboundMethodName]: 'warn'
 }
 
 const recommended = {
@@ -163,9 +164,9 @@ export default {
 		[requireHookName]: requireHook,
 		[preferTodoName]: preferTodo,
 		[preferSpyOnName]: preferSpyOn,
-		[preferTodoName]: preferTodo,
 		[preferComparisonMatcherName]: preferComparisonMatcher,
-		[preferToContainName]: preferToContain
+		[preferToContainName]: preferToContain,
+		[unboundMethodName]: unboundMethod
 	},
 	configs: {
 		all: createConfig(allRules),
